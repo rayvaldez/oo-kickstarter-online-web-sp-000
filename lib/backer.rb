02a3ffb2d372089f77adf52c8de9b15a88849fdc
Ray Valdez <ray_valdez@hotmail.co.uk>
2018-10-21 14:49:56 +0000

    automatically backed up by learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -5,10 +5,10 @@ class Backer
 
   def initialize(name)
     @name = name
+    @backed_projects = []
   end
 
   def back_project(project)
-    backed_projects = []
     backed_projects << project
   end
 end
