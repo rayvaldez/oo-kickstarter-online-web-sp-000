e9b8c7009bca8f9ffc54b6d751d6f05b28ffb195
Ray Valdez <ray_valdez@hotmail.co.uk>
2018-10-21 14:50:54 +0000

    automatically backed up by learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,7 +1,7 @@
 require 'pry'
 
 class Backer
-  attr_accessor :name
+  attr_accessor :name, :backed_projects
 
   def initialize(name)
     @name = name
@@ -9,6 +9,6 @@ class Backer
   end
 
   def back_project(project)
-    @backed_projects << project
+
   end
 end
